e8c67437625890b997400e44681ae37d5574bc82
pppp0722 <poj0722@naver.com>
2022-06-09 17:30:27 +0900

    [add] 프로필 사진 추가 및 UI 개선

--- src/body/Body.js
+++ src/body/Body.js
@@ -14,23 +14,29 @@ const Body = () => {
                         <Item>
                             <Text2>Damda</Text2>
                             <Text4>2021-12 ~ 2021-12</Text4>
+                            <br></br>
                             <Text3>Description</Text3>
                             <Text4>4컷 사진 제작 웹사이트의 프론트엔드를 구현했습니다.</Text4>
+                            <br></br>
                             <Text3>What did I do</Text3>
                             <Text4>• 설명 페이지 구현</Text4>
                             <Text4>• 모달창, 사이드바 등 기능 구현</Text4>
+                            <br></br>
                             <Text3>Tech Stack</Text3>
                             <Text4>React</Text4>
                         </Item>
-
+                        <br></br>
                         <Item>
-                            <Text2>File Viewer</Text2>
+                            <Text2><Link href='https://github.com/pppp0722/file-viewer-frontend' target='_blank'><i>File Viewer</i></Link></Text2>
                             <Text4>2021-10 ~ 2021-11</Text4>
+                            <br></br>
                             <Text3>Description</Text3>
                             <Text4>NAS에 저장되어 있는 자료들을 열람할 수 있는 웹사이트 구현 토이프로젝트의 프론트엔드와 백엔드를 구현했습니다.</Text4>
+                            <br></br>
                             <Text3>What did I do</Text3>
                             <Text4>• 트리 구조와 아이콘 방식의 프론트엔드 구현</Text4>
                             <Text4>• 파일 전송, 삭제 등 백엔드 API 구현</Text4>
+                            <br></br>
                             <Text3>Tech Stack</Text3>
                             <Text4>React, Spring</Text4>
                         </Item>
@@ -48,19 +54,16 @@ const Body = () => {
                         <Item>
                             <Text3>Java</Text3>
                             <Text4>Interface, Lambda, Generic, Stream, Optional</Text4>
-                            <Text4>Package Manage, Gradle, IntelliJ, 외부 라이브러리, Dependeny</Text4>
-                            <Text4>미니 프로젝트, OOP적 설계, flow chart, 구현</Text4>
+                            <Text4>Package Manage, IntelliJ, 외부 라이브러리</Text4>
+                            <Text4>OOP적 설계, Test</Text4>
                         </Item>
                         <Item>
                             <Text3>Database</Text3>
-                            <Text4>NoSQL, RDBMS, SQL</Text4>
+                            <Text4>RDBMS, SQL</Text4>
                         </Item>
                         <Item>
                             <Text3>Spring</Text3>
-                            <Text4>Spring MVC, Rest API, JDBC, JPA, Spring Security</Text4>
-                        </Item>
-                        <Item>
-                            <Text3>프로젝트</Text3>
+                            <Text4>Spring Container, Spring MVC, Rest API, JDBC, JPA, Spring Security</Text4>
                         </Item>
                     </Items>
                 </BigItem>
@@ -71,17 +74,25 @@ const Body = () => {
                     </ItemTitle>
                     <Items>
                         <Item>
-                            <Text3>평균 학점</Text3>
-                            <Text4>3.7</Text4>
+                            <Text3>GPA</Text3>
+                            <Text4>3.7 / 4.5</Text4>
                         </Item>
                         <Item>
-                            <Text3>프로젝트</Text3>
+                            <Text3>Project</Text3>
                             <Text4><Link href='https://github.com/pppp0722/track-me' target='_blank'><i>블루투스 기반 체크인 및 역학조사 자동화 시스템</i></Link> (2021.03 ~2021.06)</Text4>
+                            <Text5>• Description: 사용자 app, 사업자 app, 보건국 web 으로 이루어진 블루투스 기반 체크인의 코로나 역학조사 자동화 시스템</Text5>
+                            <Text5>• What did I do: 블루투스 체크인 및 블록 생성 및 검증 개발</Text5>
+                            <br></br>
                             <Text4><Link href='https://github.com/pppp0722/pallete' target='_blank'><i>퍼스널컬러 분석 및 화장품 추천 어플리케이션</i></Link> (2020.09 ~ 2020.12)</Text4>
+                            <Text5>• Description: A4용지와 함께 사진을 촬영하여 사용자의 퍼스널컬러를 분석 후 퍼스널컬러에 맞는 화장품을 추천해주는 Android app</Text5>
+                            <Text5>• What did I do: 퍼스널 컬러 분석 및 A4용지를 통한 화이트 밸런싱 개발</Text5>
+                            <br></br>
                             <Text4>AI 표정인식 식단추천 시스템 (해커톤)  (2020.07) </Text4>
+                            <Text5>• Description: 사용자의 표정, 기초 대사량, 섭취한 음식을 분석하여 다음 끼니를 추천해주는 프로그램</Text5>
+                            <Text5>• What did I do: PyQT를 사용한 Python GUI 프로그램 개발</Text5>
                         </Item>
                         <Item>
-                            <Text3>수상 경력</Text3>
+                            <Text3>AWARD</Text3>
                             <Text4>• 2021/1학기 성적2위</Text4>
                             <Text4>• 2020 제10회 창의설계경진대회 장려상, 인기상</Text4>
                             <Text4>• 2020 제8회 세종대학교 SW·AI 해커톤 금상</Text4>
@@ -102,7 +113,7 @@ const Body = () => {
                 </BigItem2>
                 <BigItem2>
                     <ItemTitle2>
-                        <Text3>Framework</Text3>
+                        <Text3>Framework & Library</Text3>
                     </ItemTitle2>
                     <Items>
                         <Text4>Spring, React</Text4>
@@ -174,7 +185,7 @@ const Text1 = styled.div`
     width: 100%;
 
     @media only screen and (max-width: 960px) {
-        font-size: 36px;
+        font-size: 32px;
     }
 `
 
@@ -202,6 +213,14 @@ const Text4 = styled.div`
     margin-bottom: 10px;
     @media only screen and (max-width: 960px) {
         font-size: 16px;
+    }
+`
+
+const Text5 = styled.div`
+    font-size: 17px;
+    margin-bottom: 10px;
+    @media only screen and (max-width: 960px) {
+        font-size: 15px;
     }
 `
 
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import githubLogo from '../images/github.jpg';
 import velogLogo from '../images/velog.jpg';
+import photo from '../images/photo.jpg';
 
 const Header = () => {
     return (
@@ -12,17 +13,23 @@ const Header = () => {
             <Mid>
                 <Text2>백엔드 개발자를 희망하고 있으며 주어진 문제를 효율적으로 해결하기 위해 고민하는 과정들을 좋아합니다. 단순 구현만 하는 개발자가 아닌, 사용하는 모든 것들을 이해하며 사용하는 개발자가 되고 싶습니다.</Text2>
             </Mid>
-            <Bottom>
-                <ImgLink href='https://github.com/pppp0722' target='_blank'>
-                    <Image src={githubLogo} href=''/>
-                </ImgLink>
-                <ImgLink href='https://velog.io/@pppp0722' target='_blank'>
-                    <Image src={velogLogo}/>
-                </ImgLink>
+            <Photo src={photo}/>
+            <Info>
+                <Bottom>
+                    <ImgLink href='https://github.com/pppp0722' target='_blank'>
+                        <Image src={githubLogo}/>
+                    </ImgLink>
+                    <ImgLink href='https://velog.io/@pppp0722' target='_blank'>
+                        <Image src={velogLogo}/>
+                    </ImgLink>
+                </Bottom>
                 <LinkDiv>
-                    Email: <Link href='mailto:poj0722@naver.com'><i>poj0722@naver.com</i></Link>
+                    <Text2><Link href='mailto:poj0722@naver.com'><i>poj0722@naver.com</i></Link></Text2>
                 </LinkDiv>
-            </Bottom>
+                <LinkDiv>
+                    <Text3>updated on 2022.06.09</Text3>
+                </LinkDiv>
+            </Info>
         </Container>
     );
 }
@@ -30,6 +37,7 @@ const Header = () => {
 const Container = styled.div`
     width: 90%;
     margin-top: 30px;
+    margin-bottom: 30px;
 `
 
 const Top = styled.div`
@@ -40,10 +48,35 @@ const Mid = styled.div`
     margin-bottom: 30px;
 `
 
+const Photo = styled.img`
+    width: 180px;
+    height: 180px;
+    margin: 0 30px 0 0;
+    border-radius: 70%;
+
+    @media only screen and (max-width: 960px) {
+        width: 120px;
+        height: 120px;
+    }
+`
+
+const Info = styled.div`
+    height: 180px;
+    float: right;
+    display: inline-block;
+    @media only screen and (max-width: 960px) {
+        height: 120px;
+        float: none;
+    }
+`
+
 const Bottom = styled.div`
     margin-bottom: 30px;
     display: flex;
     flex-wrap: wrap;
+    @media only screen and (max-width: 960px) {
+        margin-bottom: 15px;
+    }
 `
 
 const Point = styled.div`
@@ -55,43 +88,39 @@ const Text1 = styled.div`
     font-size: 70px;
     font-weight: bold;
     @media only screen and (max-width: 960px) {
-        font-size: 40px;
+        font-size: 37px;
     }
 `
 
 const Text2 = styled.div`
-    font-size: 24px;
+    font-size: 21px;
     @media only screen and (max-width: 960px) {
-        font-size: 18px;
+        font-size: 17px;
     }
 `
 
-
+const Text3 = styled.div`
+    font-size: 19px;
+    @media only screen and (max-width: 960px) {
+        font-size: 15px;
+    }
+`
 
 const Image = styled.img`
     width: 80px;
     height: 80px;
-    margin: 0 15px 0 15px;
+    margin-right: 30px;
     border-radius: 70%;
 
     @media only screen and (max-width: 960px) {
         width: 50px;
         height: 50px;
-        margin: 0 10px 0 10px;
+        margin-right: 20px;
     }
 `
 
 const LinkDiv = styled.div`
-    margin: 0 15px 0 15px;
-    font-size: 21px;
-    height: 80px;
-    line-height: 80px;
-    @media only screen and (max-width: 960px) {
-        margin: 0 10px 0 10px;
-        font-size: 18px;
-        height: 50px;
-        line-height: 40px;
-    }
+    margin-bottom: 10px;
 `
 
 const Link = styled.a`
